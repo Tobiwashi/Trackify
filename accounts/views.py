2f9733cb95bddb4bf9515f3616e70014ae52f5ca
tobiwashi <tobiigberase@gmail.com>
2023-08-18 01:27:50 -0400

    Basic Auth acquired

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,58 @@
+#Dependencies
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+#Views
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
 
 # Create your views here.
+def registerPage(request):
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+       username = request.POST.get('username')
+       password = request.POST.get('password')
+       user = authenticate(request, username=username, password=password)
+
+       if user is not None:
+            login(request, user)
+            return redirect('home')
+       else: 
+            messages.info(request, 'Username or password is incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
+
 def home(request):
+
     orders = Order.objects.all()
     customers = Customer.objects.all()
     total_customers = customers.count()
@@ -27,6 +74,7 @@ def home(request):
 
 
 def products(request):
+
     products = Product.objects.all()
 
     return render(request, 'accounts/products.html', {'products': products})
@@ -36,12 +84,16 @@ def products(request):
 
 
 def customer(request, pk):
+
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
     order_count = orders.count()
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
     context= {'customer': customer,
               'orders': orders,
               'order_count': order_count,
+              'myFilter': myFilter,
               }
     
     return render(request, 'accounts/customer.html', context)
@@ -61,6 +113,7 @@ def createOrder(request, pk):
             formset.save()
             return redirect('/')
     context ={'formset': formset}
+
     return render(request, 'accounts/order_form.html', context)
 
 
@@ -79,6 +132,7 @@ def updateOrder(request, pk):
             return redirect('/')
         
     context = {'form': form}
+
     return render(request, 'accounts/order_form.html', context)
 
 
@@ -93,4 +147,5 @@ def deleteOrder(request, pk):
         order.delete()
         return redirect('/')
     context = {'item': order}
+
     return render(request, 'accounts/delete.html', context)
